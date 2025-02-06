71605cb74f1bd9aca586bafd1ba4a693f4ef58a1
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-02-06 15:32:51 +0530

    Introduce hash method to HashMap

--- HashMap.js
+++ HashMap.js
@@ -5,4 +5,13 @@ class HashMap {
     this.#capacity = 16;
     this.#loadFactor = 0.7;
   }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNum = 33;
+    for (let i = 0; i < primeNum; i++) {
+      hashCode = (primeNum * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+    return hashCode;
+  }
 }
