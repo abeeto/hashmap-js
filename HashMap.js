1aea56cff45229efe807edc3b286ca73f0a677f6
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-02-06 15:56:54 +0530

    Add untested set fn

--- HashMap.js
+++ HashMap.js
@@ -1,9 +1,15 @@
+import LinkedList from "./LinkedList";
+
 class HashMap {
+  #map;
   #capacity;
+  #size;
   #loadFactor;
   constructor() {
     this.#capacity = 16;
     this.#loadFactor = 0.7;
+    this.#size = 0;
+    this.#map = [];
   }
 
   hash(key) {
@@ -13,5 +19,26 @@ class HashMap {
       hashCode = (primeNum * hashCode + key.charCodeAt(i)) % this.#capacity;
     }
     return hashCode;
+  }
+
+  set(key, value) {
+    let bucketIndex = this.hash(key);
+    if (bucketIndex < 0 || bucketIndex >= buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    } else if (this.#size >= Math.floor(this.#capacity * this.#loadFactor)) {
+      // TODO: find way to resize map to double capacity
+      console.log("Should resize here!");
+    }
+    let bucketList = this.#map[bucketIndex];
+    if (bucketList === undefined) {
+      bucketList = new LinkedList();
+    }
+    let currentPairValue = bucketList?.at(bucketList?.find(key));
+    if (currentPairValue) {
+      currentPairValue.value = value;
+    } else {
+      bucketList.append({ key, value });
+      this.#size = this.#size + 1;
+    }
   }
 }
