d9c97378eb0236db6be34fdd842251eff042ef18
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-02-07 19:42:37 +0530

    HashMap can retrieve value from valid key

--- HashMap.js
+++ HashMap.js
@@ -1,7 +1,7 @@
-import LinkedList from "./LinkedList";
+import LinkedList from "./LinkedList.js";
 
 class HashMap {
-  #map;
+  #buckets;
   #capacity;
   #size;
   #loadFactor;
@@ -9,7 +9,7 @@ class HashMap {
     this.#capacity = 16;
     this.#loadFactor = 0.7;
     this.#size = 0;
-    this.#map = [];
+    this.#buckets = new Array(this.#capacity);
   }
 
   hash(key) {
@@ -23,22 +23,36 @@ class HashMap {
 
   set(key, value) {
     let bucketIndex = this.hash(key);
-    if (bucketIndex < 0 || bucketIndex >= buckets.length) {
+    if (bucketIndex < 0 || bucketIndex >= this.#buckets.length) {
       throw new Error("Trying to access index out of bounds");
     } else if (this.#size >= Math.floor(this.#capacity * this.#loadFactor)) {
       // TODO: find way to resize map to double capacity
       console.log("Should resize here!");
     }
-    let bucketList = this.#map[bucketIndex];
-    if (bucketList === undefined) {
-      bucketList = new LinkedList();
+    if (this.#buckets[bucketIndex] === undefined) {
+      this.#buckets[bucketIndex] = new LinkedList();
     }
-    let currentPairValue = bucketList?.at(bucketList?.find(key));
-    if (currentPairValue) {
-      currentPairValue.value = value;
-    } else {
-      bucketList.append({ key, value });
-      this.#size = this.#size + 1;
+    let bucketList = this.#buckets[bucketIndex];
+    bucketList.append({ key, value });
+    this.#size = this.#size + 1;
+  }
+
+  get(key) {
+    let bucketIndex = this.hash(key);
+    if (bucketIndex < 0 || bucketIndex >= this.#buckets.length) {
+      throw new Error("Trying to access index out of bounds");
     }
+    let bucketList = this.#buckets[bucketIndex];
+    if (bucketList === undefined) return null;
+    bucketList.entries.forEach((node) => {
+      if (node.value.key === key) {
+        console.log(node.value.value);
+        return node.value.value;
+      }
+    });
+  }
+
+  length() {
+    return this.#size;
   }
 }
